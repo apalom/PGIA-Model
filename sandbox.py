b7f657af74d6957e41a2c97018db6efbcfd8b3f3
apalom <35828580+apalom@users.noreply.github.com>
2018-09-20 11:20:30 -0600

    KDE Tests

--- sandbox.py
+++ sandbox.py
@@ -10,13 +10,18 @@ import matplotlib.pyplot as plt
 
 maindat = dfHomeDay;
 
+hr = 3;
 
+maindat = maindat.iloc[hr]
 my_kde = st.gaussian_kde(maindat)
-sample = my_kde.resample(10000)
+sample = my_kde.resample(5000)
 
-x    = sp.linspace(sample[0,0], sample[0,-1],400)
+#x    = sp.linspace(sample[0,0], sample[0,-1], 400)
+x    = sp.linspace(0, 10, 100)
 fig  = plt.figure()
 fig1 = fig.add_subplot(111)
 plt.plot(x, my_kde(x),'b--')
+plt.hist(sample, density=True, facecolor='blue', alpha=0.2)
 plt.xlabel('Title Here ')
+plt.xlim(0, 10)
 plt.show()
