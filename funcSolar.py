d450e6dda2ae842161aed37546d26175fc2a4859
apalom <35828580+apalom@users.noreply.github.com>
2018-10-02 10:18:22 -0600

    funcSolar Added (no dist)

---
+++ funcSolar.py
@@ -0,0 +1,66 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Oct  2 08:12:09 2018
+
+@author: Alex
+"""
+
+def funcSolar(maxPV, dfSolarDay, hr, PVtoHomes, numHomes, numBuses):
+    
+    import numpy as np
+    import random
+    
+    genPV_kW = np.zeros((1, numBuses))
+    genPV_kVAR = np.zeros((1, numBuses))    
+    sampleSolar_kW = np.zeros((1, numHomes))    
+
+    # Profile from data   
+    # DNI Direct Normal Radiation [w/m2]
+    SysSize = 30; # Assume 5kW system size ~ 30 m2
+            
+  
+    for el in PVtoHomes:        
+        sampleSolar_kW[0][el] = (dfSolarDay['DNI'][hr]/1000)*SysSize
+
+    # Note Homes 1 & 2 are on bus 9, H3-4 on b5, H5-6 b7
+    # H7-8 on b8, H9-10 on b4, H11-12 on b6    
+    
+    #Bus 9 
+    bus9_kW = sampleSolar_kW[0][0] + sampleSolar_kW[0][1]
+    genPV_kW[0][8] = bus9_kW
+
+    #Bus 5 
+    bus5_kW = sampleSolar_kW[0][2] + sampleSolar_kW[0][3]
+    genPV_kW[0][4] = bus5_kW
+
+    #Bus 7 
+    bus7_kW = sampleSolar_kW[0][4] + sampleSolar_kW[0][5]
+    genPV_kW[0][6] = bus7_kW
+    
+    #Bus 3 
+    genPV_kW[0][2] = 0
+        
+    #Bus 8
+    bus8_kW = sampleSolar_kW[0][6] + sampleSolar_kW[0][7]
+    genPV_kW[0][7] = bus8_kW
+    
+    #Bus 4
+    bus4_kW = sampleSolar_kW[0][8] + sampleSolar_kW[0][9]
+    genPV_kW[0][3] = bus4_kW
+    
+    #Bus 6
+    bus6_kW = sampleSolar_kW[0][10] + sampleSolar_kW[0][11]
+    genPV_kW[0][5] = bus6_kW
+    
+    #Bus 2 
+    genPV_kW[0][1] = 0    
+
+    #Slack Bus
+    genPV_kW[0][0] = 0
+        
+    
+    # Handle negative values and calculate kVAR        
+    genPV_kW = genPV_kW.clip(min = 0)
+    genPV_kVAR = 0.01*genPV_kW
+    
+    return (genPV_kW, genPV_kVAR)
--- main.py
+++ main.py
@@ -16,9 +16,9 @@ from itertools import permutations
 timeMain = timeit.default_timer()
 
 # Load Data Function Call 
-from funcLoadData import *
-[dfSys, dfHome, dfEV, dfSolar] = funcLoadData()
-dfSys['Gen'].Pg = np.zeros((len(dfSys['Gen'].Pg)))[:]
+#from funcLoadData import *
+#[dfSys, dfHome, dfEV, dfSolar] = funcLoadData()
+#dfSys['Gen'].Pg = np.zeros((len(dfSys['Gen'].Pg)))[:]
 
 #---- Define Parameters ----#
 day = '2015-07-01'; # peak day for analysis
@@ -27,10 +27,12 @@ XFMR = 50; # Transformer rating (kVA)
 secondaryL = 100 # Meters = 328 ft
 chgrRate = 19.2; # Average charger power rating (kW)
 maxEV = 4;
+maxPV = 4;
 numHomes = 12;
 numBuses = len(dfSys['Bus'])
 numLines = len(dfSys['Branch'])
 EVstoHomes = np.random.permutation(numHomes)[0:maxEV]
+PVtoHomes = np.random.permutation(numHomes)[0:maxPV]
 
 # Filter Home Load Data for Single Day
 from funcPeakDay import *
@@ -40,6 +42,7 @@ day_P_flows = np.zeros((24,numLines))
 day_Amp_flows = np.zeros((24,numLines))
 day_Home_kW = np.zeros((24,numBuses))
 day_EV_kW = np.zeros((24,numBuses))
+day_PV_kW = np.zeros((24,numBuses))
 day_Slack_kW_kVAR = np.zeros((24,2))
 
 for hr in range(24):
@@ -54,15 +57,25 @@ for hr in range(24):
     from funcPoiss import *
     [loadEV_kW, loadEV_kVAR, EVatHome] = funcPoiss(dfEV, dfSys, maxEV, chgrRate, hr, EVstoHomes, numHomes, numBuses)
     day_EV_kW[hr,:] = loadEV_kW
+    
+    # PV Solar Gen (no distribution)
+    from funcSolar import *
+    [genPV_kW, genPV_kVAR] = funcSolar(maxPV, dfSolarDay, hr, PVtoHomes, numHomes, numBuses)    
+    day_PV_kW[hr,:] = genPV_kW
        
     # Sum Home and EV Loads
     dfSys['Bus'].Pd = (loadHome_kW + loadEV_kW)[0][:]
     dfSys['Bus'].Qd = (loadHome_kVAR + loadEV_kVAR)[0][:]
     
+    # Calculate PV Gen
+    dfSys['Gen'].Pg[1:] = genPV_kW[0][1:]
+    dfSys['Gen'].Pg[1:] = genPV_kVAR[0][1:]
+    
+    # Calculate Slack Bus Gen
     slackBus = dfSys['Gen'][dfSys['Gen'].bus == 1].index[0]
        
-    dfSys['Gen'].Pg[slackBus] = sum(sum(loadHome_kW + loadEV_kW))
-    dfSys['Gen'].Qg[slackBus] = sum(sum(loadHome_kVAR + loadEV_kVAR))
+    dfSys['Gen'].Pg[slackBus] = sum(sum(loadHome_kW + loadEV_kW - genPV_kW))
+    dfSys['Gen'].Qg[slackBus] = sum(sum(loadHome_kVAR + loadEV_kVAR - genPV_kVAR))
             
     # DC Powerflow Function Call 
     from funcDCPF import *
@@ -78,4 +91,4 @@ for hr in range(24):
 elapsedMain = timeit.default_timer() - timeMain
 print('Main time: {0:.4f} sec'.format(elapsedMain))
 
-print('\n [--- CASE: ' + str(maxEV) + 'EVs ' + str(chgrRate) + 'kW ---]')
+print('\n [--- CASE: ' + str(maxEV) + 'EVs ' + str(chgrRate) + 'kW chgr ' + str(maxPV) + 'PV ---]')
