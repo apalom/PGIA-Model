a1ecce3193c058b29e5318280e17419669bc2154
apalom <35828580+apalom@users.noreply.github.com>
2018-12-03 18:09:48 -0700

    can update hourly data by hardcoded input

--- viz/script.js
+++ viz/script.js
@@ -15,22 +15,26 @@ readData();
  */
 async function readData() {
 
-    try{
+    //try{
         const busData = await d3.csv('case12_bus.csv');
         const ampData = await d3.csv('lineAmps.csv');
 
         console.log("Bus Config: ", busData);
         console.log("Line Loading: ", ampData);
+        //console.log("Current: ", ampData);
 
-        updateData(busData)
+        updateData(busData, ampData, t)
 
-    } catch (error) {
-        alert('Could not load the dataset!');
-    }
+    // } catch (error) {
+    //     alert('Could not load the dataset!');
+    // }
 }
 
-function updateData(busData) {
+function updateData(busData, ampData, t) {
     //console.log('Line 0 tbus: ', busData[0].tbus);
+
+    //let t = 1;
+    let hr = 'hr' + t;
 
     // Set up linear SVG scales
     let xScale = d3.scaleLinear()
@@ -40,9 +44,18 @@ function updateData(busData) {
         .domain([1, 3])
         .range([0.1*height, 0.9*height]);
 
+
+    let min = d3.min(ampData, function(d) {
+        return d[hr]});
+
+    let max = d3.max(ampData, function(d) {
+        return d[hr]});
+
+    console.log('Min/Max', min, max)
+
     // Setup ColorScale
     let colorScale = d3.scaleLinear()
-        .domain([0, 300])
+        .domain([min-5, max])
         .range(['white', 'red']);
 
     let branches = d3.select('#networkSVG')   // SELECT
@@ -53,6 +66,7 @@ function updateData(busData) {
     let drawBranches = selectBranches         // ENTER
         .enter().append('line');
 
+
     //Update properties of path according to the bound data
     drawBranches
         .attr('x1', (d) => xScale(d.fromX))
@@ -60,13 +74,17 @@ function updateData(busData) {
         .attr('x2', (d) => xScale(d.toX))
         .attr('y2', (d) => yScale(d.toY))
         .attr('id', (d,i) => 'line' + (i+1))
-        .attr('stroke', (d) => colorScale(d.amps));
+        .attr('stroke', function(d,i) {
+
+            console.log(ampData[i][hr], colorScale(ampData[i][hr]))
+            return colorScale(ampData[i][hr])
+        });
 
     let labelBranches = selectBranches
         .enter().append('text');
 
     labelBranches
-        .text((d) => 'Line '+ d.line)
+        .text((d,i) => d.line + ' = ' + ampData[i][hr])
         .attr('x', (d) => 0.5*(xScale(d.fromX) + xScale(d.toX))+5)
         .attr('y', (d) => 0.5*(yScale(d.fromY) + yScale(d.toY))-10);
 
