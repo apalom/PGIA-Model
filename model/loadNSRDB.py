34314e7a379f236bd418435a2154748ef394dc2b
apalom <35828580+apalom@users.noreply.github.com>
2018-12-10 23:15:02 -0700

    Temperature data built

--- model/loadNSRDB.py
+++ model/loadNSRDB.py
@@ -5,4 +5,51 @@ Created on Mon Dec 10 18:20:47 2018
 @author: Alex Palomino
 """
 
-temp
+#%% Load Temp/Solar Data
+
+import pandas as pd
+import numpy as np
+import glob
+
+
+files = glob.glob("data/NSRDB/158327*.csv")
+
+data = {}
+yr = 2008;
+
+for file in files:
+    
+    data[yr] = pd.read_csv(file)
+    yr += 1;
+    
+#%% Parse Date
+
+#colNames = ['Year', 'Month', 'Day', 'Date', 'Hour', 'Minute', 'GHI', 'Temperature']
+tempCol = list(np.arange(2008,2018,1));
+
+col = []
+
+for year in tempCol:
+    x = ('yr'+ str(year))
+    col.append(x)
+#tempCol = 'yr'*10 + list(np.arange(2008,2018,1))
+
+  
+tempDayData = pd.DataFrame(0, index=np.arange(24), columns=col)
+
+#yr = 2007;
+month = 7;
+day = 1;
+i = 0;
+
+for key, yrData in data.items():
+    j = 'yr'+str(key)
+    print(j)
+    tempData = yrData.loc[yrData.Month == month]
+    tempData = tempData.loc[tempData.Day == day]
+    
+    tempDayData[col[i]] = tempData.GHI.values
+    i += 1;
+
+    
+    
