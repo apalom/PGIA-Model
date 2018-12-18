a8c1c9576bcf63cfd1a20b9c02b4b5f022e52784
apalom <35828580+apalom@users.noreply.github.com>
2018-12-18 12:00:46 -0800

    Update loadNSRDB.py

--- model/loadNSRDB.py
+++ model/loadNSRDB.py
@@ -32,11 +32,8 @@ for file in files:
     
     data[yr] = pd.read_csv(file)
     data[yr].Temperature = data[yr].Temperature.apply(lambda x: x*(9/5) + 32)
-    col = 'yr'+ str(yr)
-    dfTemp[col] = data[yr].Temperature
-    dfGHI[col] = data[yr].GHI
-    yr += 1;
 
+    yr += 1;
 
 #%%
 
@@ -49,23 +46,35 @@ for year in tempCol:
     x = ('yr'+ str(year))
     col.append(x)
 
-dfTemp = pd.DataFrame(0, index=np.arange(24), columns=col)
-dfGHI = pd.DataFrame(0, index=np.arange(24), columns=col)    
-    
-month = 7;
-day = 1;
+dfTempDay = pd.DataFrame(0, index=np.arange(24), columns=col)
+dfGHIDay = pd.DataFrame(0, index=np.arange(24), columns=col)    
+
+day = '2015-07-01';
+
+m = int(day[6:7]);
+d = int(day[9:10]);
 
 for key, yrData in data.items():
     j = 'yr'+str(key)
     print(j)
-    tempData = yrData.loc[yrData.Month == month]
-    tempData = tempData.loc[tempData.Day == day]
+    tempData = yrData.loc[yrData.Month == m]
+    tempData = tempData.loc[tempData.Day == d]
     
     col = 'yr'+ str(yr)
-    dfTemp[col] = tempData.Temperature.values
-    dfGHI[col] = tempData.GHI.values
+    dfTempDay[col] = tempData.Temperature.values
+    dfGHIDay[col] = tempData.GHI.values
     
     yr += 1;
+
+#%%
+
+from scipy import stats as st
+import numpy as np
+
+sampleKDE_F = dfTempDay.iloc[0]
+my_kde = st.gaussian_kde(sampleKDE_F)
+sampleKDE_F = my_kde.resample(1)[0][0]
+
 
 #    
 ##%% Parse Date
