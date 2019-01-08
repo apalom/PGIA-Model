035e2d67bebf31e4fcbfff5e0d8342f6e7dddb2c
apalom <35828580+apalom@users.noreply.github.com>
2019-01-08 11:57:32 -0700

    B8data

--- model/plotLoadHist.py
+++ model/plotLoadHist.py
@@ -13,7 +13,8 @@ bin_edges = [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14]
 n, bins, patches = plt.hist(dfHomeDay.iloc[hr,:], bins=bin_edges, density=True, rwidth=0.9, color='lightgray')
 
 font = {'family' : 'Times New Roman',
-        'size'   : 10.5}
+        'weight' : 'ultralight',
+        'size'   : 10.0}
 plt.rc('font', **font)
                             
 plt.rcParams["figure.figsize"] = [8,6]
@@ -24,7 +25,7 @@ plt.ylabel('Frequency')
 #plt.title('Loading Histogram (19:00)')
 kdeLine = dfHomeDay.iloc[hr,:].plot.kde(color='black')
 
-print(plt.rcParams["figure.figsize"])
+print(plt.rcParams)
 
 #%%
 
