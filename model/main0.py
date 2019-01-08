ea939ab28bb3d4b09be7d47d883a86294e439807
apalom <35828580+apalom@users.noreply.github.com>
2019-01-08 08:57:42 -0700

    Fix XFMR rating input

--- model/main0.py
+++ model/main0.py
@@ -16,7 +16,8 @@ import csv
 # start timer 
 timeMain = timeit.default_timer()
 
-#maxTrials = int(input('Enter Max Trials: '));
+XFMR = int(input('Enter XFMR Rating: '))
+maxTrials = int(input('Enter Max Trials: '));
 
 # Load Data Function Call 
 from funcLoadData import funcLoadData
@@ -28,7 +29,7 @@ dfSys['Gen'].Pg = np.zeros((len(dfSys['Gen'].Pg)))[:]
 day = '2015-07-01'; # peak day for analysis
 
 maxTrials = 1200;
-XFMR = 75; # Transformer rating (kVA)
+#XFMR = 75; # Transformer rating (kVA)
 XFMRlimit= 1.3 * XFMR;
 secLimit = 218 # Amps for Overload Based [218 for 4/0 AL cables in DA411]
 chgrRate = 12.9; # Average charger power rating (kW)
